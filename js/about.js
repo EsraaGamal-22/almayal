fdb5d00425ca5827566c2f57b2a8dfb9ded44f7e
ahmed hassan <ahmed.hassan@brightskiesinc.com>
2023-07-19 16:53:13 +0300

    fix services caqrs and parteners

--- js/about.js
+++ js/about.js
@@ -8,5 +8,26 @@ document.addEventListener("DOMContentLoaded", () => {
     pagination: {
       el: ".swiper-pagination",
     },
-  });
+    autoplay: false,
+    slidesPerView: 5,
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+        // spaceBetweenSlides: 50,
+      },
+
+      499: {
+        slidesPerView: 2,
+        // spaceBetweenSlides: 50,
+      },
+      999: {
+        slidesPerView: 3,
+        // spaceBetweenSlides: 50,
+      },
+      1200: {
+        slidesPerView: 5,
+        // spaceBetweenSlides: 50,
+      },
+    },
+  })
 });
